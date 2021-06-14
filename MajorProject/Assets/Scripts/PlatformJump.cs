e1464845c6e2844b17c0dbd2a7065b940fc255d9
MoniqueM93 <78805669+MoniqueM93@users.noreply.github.com>
2021-06-14 18:29:51 +0100

    ledge jump done

---
+++ MajorProject/Assets/Scripts/PlatformJump.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformJump : MonoBehaviour
+{
+    public bool birdIsThere = false;
+    public float dropTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            birdIsThere = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            birdIsThere = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) && birdIsThere == true)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            dropTime = 1f;
+        } else if (birdIsThere == false){
+            dropTime -= Time.deltaTime;
+
+            if (dropTime <= 0 && birdIsThere == false)
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                dropTime = 0f;
+            }
+        }
+
+
+        //    if (Input.GetKey(KeyCode.DownArrow))
+        //        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        //    else
+        //        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
