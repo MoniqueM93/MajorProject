7748ded15ef749a9cd4ad714391269d1d2e1830c
MoniqueM93 <78805669+MoniqueM93@users.noreply.github.com>
2021-06-21 20:32:48 +0100

    enemy radius ready

--- MajorProject/Assets/Scripts/EnemyFight.cs
+++ MajorProject/Assets/Scripts/EnemyFight.cs
@@ -2,46 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyFight : MonoBehaviour
 {
-    public Rigidbody2D enemyRB;
-    public float healthBar = 50;
-    public float deathNum = 0;
-
     public bool hasSpottedPlayer;
     public CapsuleCollider2D radiusCollide;
-    public BoxCollider2D bodyCollide;
 
     // Start is called before the first frame update
     void Start()
     {
         radiusCollide = gameObject.GetComponent<CapsuleCollider2D>();
-        bodyCollide = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D Enemy)
+    private void OnTriggerEnter2D(Collider2D radiusCol)
     {
-        if (Enemy.tag == "Bullet" && healthBar <= 10)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            healthBar -= 10;
-        }
-
-        if (Enemy.gameObject.tag.Equals("Player") && hasSpottedPlayer == false)
+        if (radiusCol.gameObject.tag.Equals("Player"))
         {
             gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
             GameManager.playerHealth += 10;
             hasSpottedPlayer = true;
         }
     }
-
 }
