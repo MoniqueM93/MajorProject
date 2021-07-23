637d9185315e277fdd112cc7ead4b36c9e720bb5
MoniqueM93 <78805669+MoniqueM93@users.noreply.github.com>
2021-07-23 17:16:36 +0100

    both characters flying complete

--- MajorProject/Assets/Scripts/FollowMovements.cs
+++ MajorProject/Assets/Scripts/FollowMovements.cs
@@ -7,16 +7,18 @@ public class FollowMovements : MonoBehaviour
     public bool isFlying = false;
     public bool isLanding;
     public bool onTheGround;
+    public bool isWalking;
 
     public playermove playerRef;
     public Transform chickAnimationGO;
     float chickAnimationGOInitial;
-
+    Animator chickAnim;
 
 
     private void Start()
     {
         chickAnimationGOInitial = chickAnimationGO.localScale.x;
+        chickAnim = GetComponentInChildren<Animator>();
     }
 
 
@@ -26,28 +28,57 @@ public class FollowMovements : MonoBehaviour
        if (playerRef.currentSpeed > 0)
       {
             chickAnimationGO.localScale = new Vector3(chickAnimationGOInitial, chickAnimationGO.localScale.y, chickAnimationGO.localScale.z);
+            isWalking = true;
+            chickAnim.SetBool("chickIsWalking", true);
         }
         else if (playerRef.currentSpeed < 0)
         {
             chickAnimationGO.localScale = new Vector3(-chickAnimationGOInitial, chickAnimationGO.localScale.y, chickAnimationGO.localScale.z);
+            isWalking = true;
+            chickAnim.SetBool("chickIsWalking", true);
         }
 
-       if (playerRef.verticalSpeed > 0)
+        if (playerRef.currentSpeed == 0)
         {
+            isWalking = false;
+            chickAnim.SetBool("chickIsWalking", false);
+        }
+
+        if (playerRef.verticalSpeed > 0)
+        {
+            isWalking = false;
+            chickAnim.SetBool("chickIsWalking", false);
+
             isFlying = true;
+            chickAnim.SetBool("chickIsFlying", true);
+
             onTheGround = false;
         } else if (playerRef.verticalSpeed < 0)
         {
+            isWalking = false;
+            chickAnim.SetBool("chickIsWalking", false);
+
             isLanding = true;
+            chickAnim.SetBool("chickIsLanding", true);
+
             onTheGround = false;
+
             isFlying = false;
+            chickAnim.SetBool("chickIsFlying", false);
         }
 
        if (playerRef.grounded == true)
         {
             onTheGround = true;
+
             isLanding = false;
+            chickAnim.SetBool("chickIsLanding", false);
+
             isFlying = false;
+            chickAnim.SetBool("chickIsFlying", false);
         }
+
+
+
     }
 }
