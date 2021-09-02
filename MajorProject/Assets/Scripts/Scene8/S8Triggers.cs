7b27ce137a42ff76b2459412a0ace8de07af9c29
MoniqueM93 <78805669+MoniqueM93@users.noreply.github.com>
2021-09-02 19:10:30 +0100

    scene 8

---
+++ MajorProject/Assets/Scripts/Scene8/S8Triggers.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S8Triggers : MonoBehaviour
+{
+    //Classes for friend A
+    public GameObject friendA;
+    public GameObject friendAFirst;
+    public DialogueTrigger friendAFirstText;
+    public GameObject friendAPrompt;
+    public bool isFirstTalkDone = false;
+
+    public GameObject friendASecond;
+    public DialogueTrigger friendASecondText;
+
+    //Claases for friend B
+    public GameObject friendB;
+    public GameObject friendBTalk;
+    public DialogueTrigger friendBTalkText;
+    public GameObject friendBPrompt;
+    public bool isSecondTalkDone = false;
+
+    //Classes for Pet Door
+    public GameObject doorTrigger;
+    public GameObject doorPrompt;
+
+    public bool allTalkDone = false;
+
+    private void Update()
+    {
+        if (allTalkDone == true)
+        {
+            doorTrigger.SetActive(true);
+            friendA.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == friendA)
+        {
+            friendAPrompt.SetActive(true);
+        }
+
+        if (collision.gameObject == friendB && isFirstTalkDone == true)
+        {
+            friendBPrompt.SetActive(true);
+        }
+
+        if (collision.gameObject == doorTrigger)
+        {
+            doorPrompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject == friendA && isFirstTalkDone == false && isSecondTalkDone == false)
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                friendAFirst.SetActive(true);
+                friendAFirstText.TriggerDialogue();
+                isFirstTalkDone = true;
+                friendB.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+
+        if (collision.gameObject == friendB)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                friendBTalk.SetActive(true);
+                friendBTalkText.TriggerDialogue();
+                isSecondTalkDone = true;
+            }
+        }
+
+        if (collision.gameObject == friendA && isFirstTalkDone == true && isSecondTalkDone == true)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                friendASecond.SetActive(true);
+                friendASecondText.TriggerDialogue();
+                allTalkDone = true;
+            }
+        }
+
+        if (collision.gameObject == doorTrigger)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                print("This scene shall change");
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == friendA)
+        {
+            friendAPrompt.SetActive(false);
+        }
+
+        if (collision.gameObject == friendB)
+        {
+            friendBPrompt.SetActive(false);
+        }
+
+        if (collision.gameObject == doorTrigger)
+        {
+            doorPrompt.SetActive(false);
+        }
+    }
+}
